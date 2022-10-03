9fc3612a58f444de8b855d008caff7f04281d340
Víctor García <vgarciaaguilar97@gmail.com>
2022-10-03 15:30:13 +0200

    Tercer commit: - Ejercicio1/Apartado1/a,b   ESTADO: TERMINADO (**Falta explicaciones, en documento word**)

--- Ejercicio1.py
+++
@@ -1,7 +0,0 @@
-import os
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms,modes
-from cryptography.hazmat.backends import default_backend
-
-backend = default_backend()
-key = os.urandom(24)
-
